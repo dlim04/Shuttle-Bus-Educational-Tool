a7464c6ddb64b88bd12b7fde8ab314e6dc3a4a1c
drlim <drlim2u@yahoo.co.uk>
2020-06-20 15:19:05 +0100

    Revert "Fixed Typo in comments"
    This reverts commit c7f98583

--- Loop.py
+++ Loop.py
@@ -9,7 +9,7 @@ class Loop(Scope):
     def __init__(self, line_number, instructions):
         """
         Constructor for the Loop object.
-        :param line_number: The line the scope is defined at
+        :param line_number: The line the function is defined at
         :param instructions: The program to be compiled
         """
         super().__init__(line_number, instructions)
