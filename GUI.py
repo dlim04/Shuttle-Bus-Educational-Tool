fd3bcace7480af0afaa5192930f6210dd6ef65f7
drlim <drlim2u@yahoo.co.uk>
2020-06-22 07:28:04 +0100

    Added catch for when the program isn't compiled correctly

--- GUI.py
+++ GUI.py
@@ -2,10 +2,14 @@ from ShuttleBus import ShuttleBus
 
 
 def gui(program):
-    code = program.get_code()
-    shuttle_bus = ShuttleBus()
-    for line in code:
-        print(shuttle_bus.move(line))
+    if not program.is_lexically_correct() or not program.is_compiled_correct():
+        print("There was an error found in the program")
+        print(program.get_error_message())
+    else:
+        code = program.get_code()
+        shuttle_bus = ShuttleBus()
+        for line in code:
+            print(shuttle_bus.move(line))
 
     input('Press enter to close window . . . ')
 
