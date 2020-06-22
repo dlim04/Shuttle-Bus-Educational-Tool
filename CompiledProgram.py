73df563f5ddaebee882d22d96d8d5841736e58db
drlim <drlim2u@yahoo.co.uk>
2020-06-22 09:43:30 +0100

    Added instance variable instructions_line_count

--- CompiledProgram.py
+++ CompiledProgram.py
@@ -2,9 +2,10 @@ class CompiledProgram:
     """
     Class to define the CompiledProgram object to be run by the shuttle bus.
     """
-    def __init__(self, lexically_correct, compiled_correct, error_message, code):
+    def __init__(self, lexically_correct, compiled_correct, error_message, instructions_line_count, code):
         """
         Constructor for the CompiledProgram object.
+        :param instructions_line_count:
         :param lexically_correct: Whether the instructions written by the user are lexically correct as a boolean
         :param compiled_correct: Whether the instructions written by the user were compiled correctly as a boolean
         :param error_message: The error message from the lexical analysis (None if lexically correct) as a string
@@ -13,6 +14,7 @@ class CompiledProgram:
         self.__lexically_correct = lexically_correct
         self.__compiled_correct = compiled_correct
         self.__error_message = error_message
+        self.__instructions_line_count = instructions_line_count
         self.__code = code
 
     def is_lexically_correct(self):
@@ -42,6 +44,13 @@ class CompiledProgram:
         :return: The compiled instructions as a 2D array
         """
         return self.__code
+
+    def get_instructions_line_count(self):
+        """
+        Getter for the number of lines written in the instructions.
+        :return: The number of lines written in the instructions as an integer
+        """
+        return self.__instructions_line_count
 
 
 if __name__ == '__main__':
